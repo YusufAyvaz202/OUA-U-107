1e01475e8fe31be452ccbbeb0858510c5ee2bafd
Emirzkool <emirrozkul@gmail.com>
2023-04-07 22:17:16 +0300

    Deneme

---
+++ Academiamania/Assets/deneme.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deneme : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("merhabalar"); 
+    }
+
+    
+}
