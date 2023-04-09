9e80934d7ee7aa350bf4af1b42c4d67aae63e73b
Yusuf Ayvaz <65962835+YusufAyvaz202@users.noreply.github.com>
2023-04-09 16:34:40 +0300

    Enemy Spawn yerleri hazırlandı

--- Academiamania/Assets/Scripts/EnemySpawn.cs
+++ Academiamania/Assets/Scripts/EnemySpawn.cs
@@ -15,7 +15,8 @@ public class EnemySpawn : MonoBehaviour
 
     void SpawnEnemy()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-10f, 10f), 0.5f, Random.Range(-10f, 10f));
+        //Vector3 spawnPosition = new Vector3(Random.Range(-26f, 65f), Random.Range(-15f, 35f), 0f);
+        Vector3 spawnPosition = new Vector3(gameObject.transform.position.x + Random.Range(-10f, 10f), gameObject.transform.position.y + Random.Range(-10f, 10f), 0f);
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 
